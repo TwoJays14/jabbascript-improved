6af2530a5b968af3c092b6f961bd778eacf52f95
JJatau <jjatau14@outlook.com>
2024-11-03 20:53:09 +0000

    add logo image

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,17 +1,21 @@
 import {Component, OnInit, signal, WritableSignal} from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {NgOptimizedImage} from "@angular/common";
+import {LOGO_PATH} from "../../constants/image-paths";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    NgOptimizedImage
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent implements OnInit {
-  isLoggedIn: WritableSignal<boolean> = signal(false);
+  public readonly logoPath: string = LOGO_PATH;
+  public isLoggedIn: WritableSignal<boolean> = signal(false);
 
   ngOnInit(): void {
     console.log("init");
