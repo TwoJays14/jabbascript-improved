0631f82a7e52e1a7e7b0d4ecfdd4db8985211dbc
Jermaine Jatau <jjatau14@outlook.com>
2024-11-03 01:19:28 +0000

    add main pages and routing

---
+++ src/app/pages/fighting/fighting.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fighting',
+  standalone: true,
+  imports: [],
+  templateUrl: './fighting.component.html',
+  styleUrl: './fighting.component.css'
+})
+export class FightingComponent {
+
+}
---
+++ src/app/pages/quest/quest.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quest',
+  standalone: true,
+  imports: [],
+  templateUrl: './quest.component.html',
+  styleUrl: './quest.component.css'
+})
+export class QuestComponent {
+
+}
