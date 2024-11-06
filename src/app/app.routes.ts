2e3c1166d2cae7e2f82b92daf3b37931d0929824
JJatau <jjatau14@outlook.com>
2024-11-06 22:50:48 +0000

    lazy load components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
 import {ErrorComponent} from "./pages/error/error.component";
 import {FightingComponent} from "./pages/fighting/fighting.component";
@@ -8,11 +8,30 @@ import {LeaderboardComponent} from "./pages/leaderboard/leaderboard.component";
 import {LoginComponent} from "./pages/login/login.component";
 
 export const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'fighting', loadComponent: () => import('./pages/fighting/fighting.component').then(m => m.FightingComponent)},
-  {path: 'quest', component: QuestComponent},
-  {path: 'leaderboard', component: LeaderboardComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
-  {path: '**', component: ErrorComponent}
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'fighting', loadComponent: () => import('./pages/fighting/fighting.component').then(m => m.FightingComponent)
+  },
+  {
+    path: 'quest',
+    loadComponent: () => import('./pages/quest/quest.component').then(m => m.QuestComponent)
+  },
+  {
+    path: 'leaderboard',
+    loadComponent: () => import('./pages/leaderboard/leaderboard.component').then(m => m.LeaderboardComponent)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/register/register.component').then(m => m.RegisterComponent)
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./pages/error/error.component').then(m => m.ErrorComponent)
+  },
 ];
