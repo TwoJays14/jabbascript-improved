6c001663568cd4a071814b00f59d5cd28bdc6a90
JJatau <jjatau14@outlook.com>
2024-11-06 22:42:13 +0000

    style homepage

--- src/app/layouts/homeLayout/homeLayout.component.ts
+++ src/app/layouts/homeLayout/homeLayout.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {NavbarComponent} from "../../components/navbar/navbar.component";
+import {HeroComponent} from "../../components/hero/hero.component";
 
 @Component({
   selector: 'app-homeLayout',
   standalone: true,
   imports: [
-    NavbarComponent
+    NavbarComponent,
+    HeroComponent
   ],
   templateUrl: './homeLayout.component.html',
   styleUrl: './homeLayout.component.css'
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "koding-kombat": "url('/assets/background.webp')"
+      }
+    },
   },
   plugins: [require('daisyui')],
 }
